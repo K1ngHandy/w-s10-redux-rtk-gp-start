305b39bd839b62cb40c57746cb9a1262d949af02
Stephen Handy <K1ngHandy@users.noreply.github.com>
2025-11-28 19:32:50 -0500

    create initial state with redux

--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -1,42 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 
 const StyledTodo = styled.li`
-  text-decoration: ${pr => pr.$complete ? 'line-through' : 'initial'};
-  cursor: pointer;
-`
-let id = 1
-const getNextId = () => id++
+	text-decoration: ${(pr) => (pr.$complete ? 'line-through' : 'initial')};
+	cursor: pointer;
+`;
 
 export default function Todo() {
-  const todos = [ // TODO: this must come from app state!
-    { id: getNextId(), label: 'Laundry', complete: true },
-    { id: getNextId(), label: 'Groceries', complete: false },
-    { id: getNextId(), label: 'Dishes', complete: false },
-  ]
-  const showCompletedTodos = true // TODO: this must come from app state!
-  // TODO: enable ability to complete a todo!
-  // TODO: enable toggling visibility of complete todos!
+	const todos = useSelector((st) => st.todosState.todos);
+	const showCompletedTodos = useSelector(
+		(st) => st.todosState.showCompletedTodos
+	); // TODO: this must come from app state!
+	// TODO: enable ability to complete a todo!
+	// TODO: enable toggling visibility of complete todos!
 
-  return (
-    <div id="todos">
-      <h3>Todos</h3>
-      <ul>
-        {
-          todos
-            .filter(todo => {
-              return showCompletedTodos || !todo.complete
-            })
-            .map(todo => (
-              <StyledTodo $complete={todo.complete} key={todo.id}>
-                <span>{todo.label}{todo.complete && ' ✔️'}</span>
-              </StyledTodo>
-            ))
-        }
-      </ul>
-      <button>
-        {showCompletedTodos ? 'Hide' : 'Show'} completed todos
-      </button>
-    </div>
-  )
+	return (
+		<div id="todos">
+			<h3>Todos</h3>
+			<ul>
+				{todos
+					.filter((todo) => {
+						return showCompletedTodos || !todo.complete;
+					})
+					.map((todo) => (
+						<StyledTodo
+							$complete={todo.complete}
+							key={todo.id}
+						>
+							<span>
+								{todo.label}
+								{todo.complete && ' ✔️'}
+							</span>
+						</StyledTodo>
+					))}
+			</ul>
+			<button>
+				{showCompletedTodos ? 'Hide' : 'Show'} completed todos
+			</button>
+		</div>
+	);
 }
---
+++ frontend/state/todosSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+let id = 1;
+const getNextId = () => id++;
+
+export const todosSlice = createSlice({
+	name: 'todos',
+	initialState: {
+		todos: [
+			{ id: getNextId(), label: 'Laundry', complete: true },
+			{ id: getNextId(), label: 'Groceries', complete: false },
+			{ id: getNextId(), label: 'Dishes', complete: false },
+		],
+		showCompletedTodos: true,
+	},
+	reducers: {
+		toggleShowCompletedTodos: (state) => {
+			return { ...state, showCompletedTodos: !state.showCompletedTodos };
+		},
+	},
+});
+
+export default todosSlice.reducer;
